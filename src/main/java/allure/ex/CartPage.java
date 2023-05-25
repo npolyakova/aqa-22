5ed4f7149e536d9fc43429f1407b295e630d49da
n.polyakova <natli.d26.l8@gmail.com>
2023-05-25 18:14:47 +0300

    Initial commit

---
+++ src/main/java/allure/ex/CartPage.java
@@ -0,0 +1,12 @@
+package allure.ex;
+
+import org.openqa.selenium.By;
+
+public class CartPage {
+
+    public By buttonRemove = By.id("remove-sauce-labs-backpack");
+
+    public By blockCartItem = By.className("cart_item");
+
+    public By blockRemovedCartItem = By.className("removed_cart_item");
+}
